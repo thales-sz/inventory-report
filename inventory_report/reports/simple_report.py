317f322a6a65e208418e4227a9a475ab00b4c28e
Thales <thales.souz@outlook.com>
2023-01-31 16:39:19 -0300

    feat: req 02, faz a verificação de validade dos produtos

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,24 +1,45 @@
+from datetime import datetime
+
+
 class SimpleReport:
     @staticmethod
-    def generate(data: list[dict]) -> str:
-        oldest = None
-        nearest = None
-        company = None
-        max = 0
-        for product in data:
-            data_de_fabricacao = product["data_de_fabricacao"]
-            data_de_validade = product["data_de_validade"]
-            nome_da_empresa = product["nome_da_empresa"]
-            if not oldest or data_de_fabricacao < oldest:
-                oldest = data_de_fabricacao
-            if not nearest or data_de_validade < nearest:
-                nearest = data_de_validade
-            company_count = data.count(product)
-            if company_count > max:
-                max = company_count
-                company = nome_da_empresa
+    def generate(data):
+        fabricaçao = None
+        validade = None
+        empresa = None
+        hoje = datetime.now().date()
+        datas_fabricacao = [
+            datetime.strptime(produto["data_de_fabricacao"], "%Y-%m-%d").date()
+            for produto in data
+            if datetime.strptime(
+                produto["data_de_validade"], "%Y-%m-%d"
+            ).date()
+            >= hoje
+        ]
+        if datas_fabricacao:
+            fabricaçao = min(datas_fabricacao)
+        datas_validade = [
+            datetime.strptime(produto["data_de_validade"], "%Y-%m-%d").date()
+            for produto in data
+            if datetime.strptime(
+                produto["data_de_validade"], "%Y-%m-%d"
+            ).date()
+            >= hoje
+        ]
+        if datas_validade:
+            validade = min(datas_validade)
+        empresas = [
+            produto["nome_da_empresa"]
+            for produto in data
+            if datetime.strptime(
+                produto["data_de_validade"], "%Y-%m-%d"
+            ).date()
+            >= hoje
+        ]
+        if empresas:
+            empresa = max(set(empresas), key=empresas.count)
         return f"""
-        Data de fabricação mais antiga: {oldest}\n
-        Data de validade mais próxima: {nearest}\n
-        Empresa com mais produtos: {company}
+        Data de fabricação mais antiga: {fabricaçao.strftime('%Y-%m-%d')}\n
+        Data de validade mais próxima: {validade.strftime('%Y-%m-%d')}\n
+        Empresa com mais produtos: {empresa}
         """
