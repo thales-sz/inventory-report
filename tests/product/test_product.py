d9abe7b39b217afe19060c363c0a780a16a9797e
Thales <thales.souz@outlook.com>
2023-01-31 15:14:30 -0300

    test: função para o teste da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,10 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+'''from tests.factories.product_factory import ProductFactory'''
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(5, "Batata", "Donalds", "11/10", "31/10", 5, "G")
+
+    assert product.nome_do_produto == "Batata"
+    assert product.nome_da_empresa == "Donalds"
+    assert product.data_de_fabricacao == "11/10"
