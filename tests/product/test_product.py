288686a04033e2a231611f131a73d5291d55190d
Thales <thales.souz@outlook.com>
2023-01-31 15:16:34 -0300

    test: função para o teste da classe Product corrigido

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -5,6 +5,10 @@ from inventory_report.inventory.product import Product
 def test_cria_produto():
     product = Product(5, "Batata", "Donalds", "11/10", "31/10", 5, "G")
 
+    assert product.id == 5
     assert product.nome_do_produto == "Batata"
     assert product.nome_da_empresa == "Donalds"
     assert product.data_de_fabricacao == "11/10"
+    assert product.data_de_validade == "31/10"
+    assert product.numero_de_serie == 5
+    assert product.instrucoes_de_armazenamento == "G"
