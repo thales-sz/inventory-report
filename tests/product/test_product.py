cb3392cf76d56f64f76b6bec667ef18fa3e51933
Thales <thales.souz@outlook.com>
2023-02-01 10:05:51 -0300

    feat: req 08

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,6 @@
 from inventory_report.inventory.product import Product
-'''from tests.factories.product_factory import ProductFactory'''
+
+"""from tests.factories.product_factory import ProductFactory"""
 
 
 def test_cria_produto():
@@ -12,3 +13,18 @@ def test_cria_produto():
     assert product.data_de_validade == "31/10"
     assert product.numero_de_serie == 5
     assert product.instrucoes_de_armazenamento == "G"
+
+
+def test_relatorio_produto():
+    product = Product(5, "Batata", "Donalds", "11/10", "31/10", 5, "G")
+
+    assert (
+        product.__repr__()
+        == (
+            f"O produto {product.nome_do_produto}"
+            f" fabricado em {product.data_de_fabricacao}"
+            f" por {product.nome_da_empresa} com validade"
+            f" até {product.data_de_validade}"
+            f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+        )
+    )
