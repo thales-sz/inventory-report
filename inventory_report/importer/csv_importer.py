16624a9cfb43736bbd6c27d0c4ee5138bdec5472
Thales <thales.souz@outlook.com>
2023-02-01 09:37:15 -0300

    fix: chamado metodo correto na classe abstrata Importer

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -1,9 +1,10 @@
 from .importer import Importer
+from inventory_report.inventory.inventory import Inventory
 
 
 class CsvImporter(Importer):
     @classmethod
     def import_data(file):
         if file.endswith(".csv"):
-            return super().import_data(file)
+            return Inventory.verify_archive_format(file)
         raise ValueError("Arquivo inválido")
--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,9 +1,10 @@
 from .importer import Importer
+from inventory_report.inventory.inventory import Inventory
 
 
-class CsvImporter(Importer):
+class XmlImporter(Importer):
     @classmethod
     def import_data(file):
         if file.endswith(".xml"):
-            return super().import_data(file)
+            return Inventory.verify_archive_format(file)
         raise ValueError("Arquivo inválido")
